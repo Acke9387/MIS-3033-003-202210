cbc093e6bffb64481100b23844f08bdceea58844
Ackerman, Adam P <adamackrmn@gmail.com>
2022-08-29 15:28:49 -0500

    Started the WPF-Classes particiaption

--- Participations/WPF-Classes/MainWindow.xaml.cs
+++ Participations/WPF-Classes/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WPF_Classes
 {
@@ -23,6 +25,26 @@ namespace WPF_Classes
         public MainWindow()
         {
             InitializeComponent();
+
+
+            Toy myToy = new Toy();
+            myToy.Manufacturer = "Hasbro";
+            myToy.Name = "Buzz";
+            //...
+
+            var uri = new Uri("https://images-na.ssl-images-amazon.com/images/I/71rL5zB1UZL.__AC_SX300_SY300_QL70_FMwebp_.jpg");
+            var img = new BitmapImage(uri);
+
+            imgPicture.Source = img;
+
+            lstToys.Items.Add(myToy);
+        }
+
+        private void lstToys_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstToys.SelectedItem;
+
+            MessageBox.Show($"{selectedToy.Name}");
         }
     }
 }
