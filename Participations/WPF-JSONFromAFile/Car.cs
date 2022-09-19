51b36049888bcac74b5c995f8cb23ced6bd9e664
Ackerman, Adam P <adamackrmn@gmail.com>
2022-09-19 15:20:41 -0500

    Completed participation

---
+++ Participations/WPF-JSONFromAFile/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_JSONFromAFile
+{
+    public class Car
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
+        public Car()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Make = string.Empty;
+            Model = string.Empty;
+            Year = 0;
+            Color = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} has a {Color} {Year.ToString("G0")} {Make} {Model}";
+        }
+    }
+}
---
+++ Participations/WPF-JSONFromAFile/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_JSONFromAFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Car> allData = new List<Car>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            string data = File.ReadAllText("Mock_Data_Car_Owners.json");
+            allData = JsonConvert.DeserializeObject<List<Car>>(data);
+
+
+            cboColors.Items.Add("All Colors");
+            cboColors.SelectedIndex = 0;
+            List<string> colors = new List<string>();
+
+            foreach (Car car in allData)
+            {
+                lstCars.Items.Add(car);
+
+                if (!colors.Contains(car.Color))// !cboColors.Items.Contains(car.Color) )
+                {
+                    //cboColors.Items.Add(car.Color); 
+                    colors.Add(car.Color);
+                }
+            }
+
+            foreach (string color in colors.OrderBy(x => x))
+            {
+                cboColors.Items.Add(color);
+            }
+            lblResults.Content = $"There are {lstCars.Items.Count.ToString("N0")} cars";
+        }
+
+        private void cboColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedColor = (string)cboColors.SelectedItem;
+
+            lstCars.Items.Clear();
+
+            foreach (Car car in allData)
+            {
+                if (selectedColor == car.Color || selectedColor == "All Colors")
+                {
+                    lstCars.Items.Add(car);
+                }
+            }
+
+            lblResults.Content = $"There are {lstCars.Items.Count.ToString("N0")} {selectedColor} cars";
+        }
+    }
+}
