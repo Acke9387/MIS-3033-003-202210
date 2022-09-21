193be304c350e954b6371b4a9e52d7b96c0d4021
Ackerman, Adam P <adamackrmn@gmail.com>
2022-09-21 16:06:56 -0500

    Did an in class assignment over json from a web service

---
+++ In_Class_Examples/JSON_RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string url = "https://rickandmortyapi.com/api/character";
+
+            HttpClient client = new HttpClient();
+            string json = client.GetStringAsync(url).Result;
+
+            RickAndMortyAPI data = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+            foreach (Character character in data.results)
+            {
+                cboCharacters.Items.Add(character); 
+            }
+        }
+
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Character selected = (Character)cboCharacters.SelectedItem;
+
+            img.Source = new BitmapImage(new Uri(selected.image));
+            lblName.Content = selected.name;
+        }
+    }
+}
