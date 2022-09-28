ea4b2412b76fa0f55bbcb753f75e8f9aa374ae94
Ackerman, Adam P <adamackrmn@gmail.com>
2022-09-28 15:42:00 -0500

    completed the pokemon participation

--- Participations/JSON-Pokemon/MainWindow.xaml.cs
+++ Participations/JSON-Pokemon/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,78 @@ namespace JSON_Pokemon
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PokemonDetails pokemon = null;
+        private bool showFront = true;
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            string url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1200";
+            PokemonAPI api = null;
+
+            using (var client = new HttpClient())
+            {
+
+                HttpResponseMessage response = client.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var json = response.Content.ReadAsStringAsync().Result;
+
+                    api = JsonConvert.DeserializeObject<PokemonAPI>(json);
+
+                }
+
+            }
+
+            foreach (ResultItem pokemon in api.results.OrderBy(x => x.name))
+            {
+                lstPokemon.Items.Add(pokemon);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultItem seleted = (ResultItem)lstPokemon.SelectedItem;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(seleted.url).Result;
+
+                pokemon = JsonConvert.DeserializeObject<PokemonDetails>(json);
+                txtHeight.Text = pokemon.height.ToString();
+                txtWeight.Text = pokemon.weight.ToString();
+                txtName.Text = pokemon.name;
+
+                img.Source = new BitmapImage(new Uri(pokemon.sprites.front_default));
+                showFront = false;
+
+            }
+        }
+
+        private void btnFlip_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (showFront)
+            {
+                img.Source = new BitmapImage(new Uri(pokemon.sprites.front_default));
+                showFront = false;
+            }
+            else
+            {
+                if (pokemon.sprites.back_default != null)
+                {
+                    img.Source = new BitmapImage(new Uri(pokemon.sprites.back_default)); 
+                }
+                showFront = true;
+            }
+           
+
+
         }
     }
 }
---
+++ Participations/JSON-Pokemon/PokemonDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Pokemon
+{
+    public class PokemonDetails
+    {
+
+        public int weight {get;set;}
+        public int height { get; set; }
+
+
+        public string name { get; set; }
+
+
+        public Sprite sprites { get; set; }
+
+    }
+
+    public class Sprite
+    {
+        public string front_default { get; set; }
+
+        public string back_default { get; set; }
+    }
+}
