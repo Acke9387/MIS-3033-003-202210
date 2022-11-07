5d81b3a8381a8752394af91cc9fa92e9c4794f87
Ackerman, Adam P <adamackrmn@gmail.com>
2022-11-07 15:47:21 -0600

    Completed an in class example using ASP.NET and MVC

---
+++ In_Class_Examples/MyFirstMVCApplication/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstMVCApplication.Models;
+
+namespace MyFirstMVCApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TeachingAssistants()
+        {
+            List<TA> tas = new List<TA>()
+            {
+                new TA()
+                {
+                    Name = "Matt"
+                },
+                new TA()
+                {
+                    Name = "Connor"
+                },
+                new TA()
+                {
+                    Name = "Jenna"
+                }
+            };
+
+            return View(tas);
+        }
+    }
+}
---
+++ In_Class_Examples/MyFirstMVCApplication/Models/TA.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace MyFirstMVCApplication.Models
+{
+    public class TA
+    {
+        [DisplayName("TA Name")]
+        public string Name { get; set; }
+    }
+}
